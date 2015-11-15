02bedf2722fe782c92750d21b897676c54b88757
tt24 <tt24@st-andrews.ac.uk>
2015-11-15 17:00:53 +0000

    implementing state formulas

--- src/lsv/core/SimpleModelChecker.java
+++ src/lsv/core/SimpleModelChecker.java
@@ -1,6 +1,7 @@
 package lsv.core;
 
 import lsv.model.Model;
+import lsv.model.State;
 import lsv.grammar.Formula;
 
 public class SimpleModelChecker implements ModelChecker {
@@ -8,6 +9,32 @@ public class SimpleModelChecker implements ModelChecker {
 	public boolean check(Model model, Formula constraint, Formula formula) {
 		// TO IMPLEMENT
 		return false;
+	}
+
+	public boolean checkAP(Formula formula, State state) {
+		String ap = formula.getAp()[0];
+		for (String label : state.getLabel()) {
+			if (label.equals(ap)) {
+				if (formula.getApNeg()[0]) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}
+		if(formula.getApNeg()[0]) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkTautology(Formula formula) {
+		if(formula.getTautology()[0].equals("True")) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	public String[] getTrace() {
--- src/lsv/core/TransitionGraphNode.java
+++
@@ -1,5 +0,0 @@
-package lsv.core;
-
-public class TransitionGraphNode {
-
-}
